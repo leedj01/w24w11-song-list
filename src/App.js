2add9d7ae58ecba0cb31db900869576b01b16642
RRRdj <emforhs3213@naver.com>
2024-11-18 19:00:07 +0900

    🚧 Lambda Expression 사용

--- src/App.js
+++ src/App.js
@@ -1,61 +1,37 @@
 import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header/>
       <Playlist/>
-      <Container/>
-      <Container2/>
-      <Container3/>
+      <Container title="사랑에 연습이 있었다면" index="1"/>
+      <Container title="사건의 지평선" index="2"/>
+      <Container title="사랑은 늘 도망가" index="3"/>
     </div>
   )
 }
 
-function Header() {
+const Header = () => {
   return (
       <h1>React 프로그래밍</h1>
   )
 }
 
-function Playlist() {
+const Playlist = () => {
   return (
       <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>
   )
 }
 
-function Container() {
+const Container = (props) => {
   return (
     <div className="container">
-      <a href="https://www.youtube.com/results?search_query=사랑에 연습이 있었다면">
+      <a href={`https://www.youtube.com/results?search_query=${props.title}`}>
         <img 
-          src="https://picsum.photos/600/150?random=1" 
-          alt="랜덤 이미지 1"/>
-        <div className="song-title">사랑에 연습이 있었다면</div>
-      </a>
-    </div>
-  )
-}
-
-function Container2() {
-  return (
-    <div className="container">
-      <a href="https://www.youtube.com/results?search_query=사건의 지평선">
-        <img 
-          src="https://picsum.photos/600/150?random=2" 
-          alt="랜덤 이미지 2"/>
-        <div className="song-title">사건의 지평선</div>
-      </a>
-    </div>
-  )
-}
-
-function Container3() {
-  return (
-    <div className="container">
-      <a href="https://www.youtube.com/results?search_query=사랑은 늘 도망가">
-        <img src="https://picsum.photos/600/150?random=3" alt="랜덤 이미지 3"/>
-        <div className="song-title">사랑은 늘 도망가</div>
+          src={`https://picsum.photos/600/150?random=${props.index}`} 
+          alt={`랜덤 이미지 ${props.index}`}/>
+        <div className="song-title">{props.title}</div>
       </a>
     </div>
   )
